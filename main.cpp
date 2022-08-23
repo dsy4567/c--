61260713257db2ae132afcddd8e970b0fe946814
dsy4567 <dsy4567@outlook.com>
2022-08-23 11:30:21 +0800

    .

--- main.cpp
+++ main.cpp
@@ -1,19 +1,41 @@
 ﻿#include <iostream>
-#include <cmath>
 
 using namespace std;
 
-typedef long long ll;
-int A[101][101], B[101][101];
-int m, n;
+int n, m, img[101][101];
 char s[101];
 
+void ssz90(){
+
+}
+void nsz90(){
+
+}
+void sxfz(){
+
+}
+void zyfz(){
+
+}
 int main()
 {
-	freopen("in.txt", "r", stdin);
-	freopen("out.txt", "w", stdout);
-	
-	return 0;
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= m; j++)
+        {
+            cin >> img[i][j];
+        }
+    }
+    cin >> s;
+
+    for (int i = 1; i <= 100; i++)
+    {
+        cout << s[i];
+    }
+    
+
+    return 0;
 }
 
 /*
@@ -37,8 +59,8 @@ flags（标识）	描述
 +	    强制在结果之前显示加号或减号（+ 或 -），即正数前面会显示 + 号。默认情况下，只有负数前面会显示一个 - 号。
 空格	 如果没有写入任何符号，则在该值前面插入一个空格。
 #	    与 o、x 或 X 说明符一起使用时，非零值前面会分别显示 0、0x 或 0X。
-		与 e、E 和 f 一起使用时，会强制输出包含一个小数点，即使后边没有数字时也会显示小数点。默认情况下，如果后边没有数字时候，不会显示显示小数点。
-		与 g 或 G 一起使用时，结果与使用 e 或 E 时相同，但是尾部的零不会被移除。
+        与 e、E 和 f 一起使用时，会强制输出包含一个小数点，即使后边没有数字时也会显示小数点。默认情况下，如果后边没有数字时候，不会显示显示小数点。
+        与 g 或 G 一起使用时，结果与使用 e 或 E 时相同，但是尾部的零不会被移除。
 0	    在指定填充 padding 的数字左边放置零（0），而不是空格（参见 width 子说明符）。
 
 width（宽度）	描述
